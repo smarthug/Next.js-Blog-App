4618aeeaba106355b15c39861c7e4bb1028cb985
smarthug <kirklayer@gmail.com>
2024-01-26 19:08:23 +0900

    good gangs

---
+++ components/opinion/goodgang.js
@@ -0,0 +1,219 @@
+import Image from 'next/image';
+
+const MainAboutPage = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold my-6">클레이튼-핀시아 개정안, &quot;반전&quot;인가 &quot;배신&quot;인가</h1>
+            <br /><br />
+            <p className="mb-4">2024년 1월 26일, 드디어 클레이튼(Klaytn)과 핀시아(Finschia)의 통합 프로젝트인 &quot;프로젝트 드래곤(PDT)&quot; 제안의 거버넌스 투표가 시작되었습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">투표가 있기 하루 전, 핀시아 재단에서는 지난 19일 치뤄진 AMA에서의 약속대로 통합 개정안을 내놨습니다. 개정안의 주요 내용은 다음과 같습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">1. FNSA:PDT의 교환비 및 생태계 기여자 보상 미비 -&gt; 온체인 기여자 보상 8,000만 PDT<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">2. 통합 후 KLAY와 FNSA의 공급량 차이로 인한 유저 거버넌스 차이 -&gt; 최대 4,000만 PDT를 핀시아 거버넌스 3개 참가사에 지원함.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">3. Dapp 마이그레이션 등 통합에 따른 비용 지출 지원 없음 -&gt; 총 2,500만 PDT를 마이그레이션 비용 및 Dapp유저들을 위한 리텐션 지원.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">19일에 치뤄진 AMA에서 버그홀측의 질의 내용과 의견이 상당부분 반영된 것으로&quot;는&quot; 보입니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">다만, 실제로 발표된 수치들을 대입한 결과에 대해서 핀시아의 홀더들은 대부분 낙담하고 있습니다.<br /><br /></p>
+            <br /><br />
+            <h3 className="text-2xl font-bold my-6">1. 실제 교환비는 1:148 -&gt; 1:168이다.<br /><br /></h3>
+            <br /><br />
+            <p className="mb-4">위에서 말씀드린 온체인 기여자 보상인 8천만 PDT를 핀시아의 공급량에 따라 계산해보면, 현재 핀시아 노드에 스테이킹한 홀더에게만 약 1:168의 교환비가 적용된다고 합니다. 종전 기획기사에서 보여드렸던 핀시아 홀더들의 의견에 다소 교환비가 상향되긴 했습니다만, &quot;두 배라도 안된다&quot;라는 의견에는 상당히 못미치는 교환비입니다.<br /><br /></p>
+            <br /><br />
+            <h3 className="text-2xl font-bold my-6">2. Dapp 마이그레이션을 위한 2,500만 PDT는 실제로 대부분 굳갱랩스에 전달된다.<br /><br /></h3>
+            <br /><br />
+            <p className="mb-4">굳갱랩스의 디스코드 발표에 따르면, 개정안을 통해 밝혀진 마이그레이션 등 통합 총 비용인 2,500만 PDT 중 1,500만 PDT를 굳갱이 확보하였다고 합니다. 굳갱은 디스코드를 통해서 16일 통합 제안 발표 이후 줄곧 커뮤니티의 의사를 반영해 &quot;강력한 반대&quot;라고 밝혀왔습니다만, 이러한 재원 확보 이후에는 돌연 &quot;통합 찬성&quot;의견으로 바뀌었습니다. 이에 대해서는 아래에서 더 자세히 다루도록 하겠습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">개정안의 발표에도 불구하고 아직도 FNSA 홀더들의 의견이 제대로 반영되지 않는다는 기조가 디스코드에 흐르면서, 자연스럽게 26일부터 치뤄질 거버넌스 투표는 부결될 것으로 비춰졌습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">하지만, 굳갱랩스(GoodGang Labs)의 갑작스러운 찬성 의견 발표 이후 분위기는 삽시간에 바뀌었습니다. 이를 시간 순서대로 보면 다음과 같습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">1. 1월 16일 ~ 1월 18일, 강력한 반대 의사 표시<br /><br /></p>
+            <br />
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/1.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+                
+            </div>
+
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/02.JPG"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/3.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/4.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/5.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/6.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+
+            <br />
+            <p className="mb-4">위 첨부한 사진과 같이, 굳갱랩스의 대표는 디스코드에서 커뮤니티의 의견을 적극 반영하여 &quot;강력한 반대&quot;임을 알렸고, 이후 프로포절의 업데이트가 이뤄진다면 비어갱 커뮤니티와 논의 및 의견 수렴 후 최종 의사결정을 내릴 것이라고 밝혔습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">2. 1월 19일, 커뮤니티 의견을 적극 반영할 것이지만?<br /><br /></p>
+
+            <br />
+          
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/7.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/8.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/9.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/10.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            
+            <br />
+            <p className="mb-4">1월 18일에도 밝혔듯이, 굳갱랩스의 대표는 &quot;프로포절 업데이트 시, 커뮤니티의 의견 듣고, 내부 논의 후 의사 결정을 할 예정이다. 팀과 커뮤니티의 의사 결정이 다르다면, 재단에 요청하여 굳갱노드에 FNSA를 예치한 스테이커들이 다른 노드로 재예치할 시간을 확보할 것이며, 재단이 요청을 받아들이지 않는다면 비어갱 홀더 전체 투표를 통해 의사결정을 진행할 것&quot;이라고 발표하였습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">19일에는 이를 다시 한번 언급하였으나, 그 이후의 발언이 다소 모호합니다. &quot;굳갱랩스는 현재 DAO 형태로 운영하는 것이 아니라, 프로젝트를 주도적으로 리드하는 책임경영을 하고 있다.&quot;라는 굳갱랩스 대표의 말은, 마치 &quot;최종 선택권은 커뮤니티가 아닌 우리에게 있다.&quot;라고 통보하는 어조로 보일 수 있습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">다만, 팀과 커뮤니티 간의 의견 차이가 발생한다면, 스테이커들의 언스테이킹 기간을 확보할 예정이며, 기간 확보가 되지 않는다면 전체 투표를 진행할 것이라는 의사에는 변함이 없었습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4 text-1xl font-bold my-6">3. 1월 25일, 돌연 &quot;찬성&quot;의견 발표<br /><br /></p>
+
+            <br />
+            
+            <div
+                className="flex justify-center items-center my-4"
+            >
+                
+                 <Image
+                    src="https://lirimnaqhglyehdfwelj.supabase.co/storage/v1/object/public/goodgang/11.png"
+                    alt="1"
+                    width={1920}
+                    height={1080}
+                />
+                
+            </div>
+            <br />
+            <p className="mb-4">1월 25일, 굳갱랩스 대표는 돌연 통합 건에 대한 &quot;찬성&quot; 의견을 밝혔습니다. 투표를 하루 앞두고 이뤄진 이 발표에 대해 핀시아 홀더들은 대단히 황당하다는 분위기입니다. 찬성의 이유는 다음과 같습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">첫째, 굳갱랩스와 커뮤니티를 위한 마이그레이션 재원 한화 약 37억 원(1500만 PDT)을 확보하였습니다. 또한 금번 통합 건에 대한 굳갱랩스의 의견을 지지해주신 굳갱랩스 노드 스테이커, 비어갱 홀더 등을 포함한 커뮤니티를 위한 보상 및 프로젝트의 장기적 성장을 위해 해당 재원의 70%를 사용하기로 결정하였습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">둘째, 글로벌 경쟁과 생존을 도모하기 위해 &quot;판을 키워야 한다&quot;는 양 재단의 의견에 공감하였습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">두번째 이유는 핀시아 홀더나 클레이튼 홀더나 모두 공감할 것입니다. 하지만, 현재 핀시아 홀더 전체의 의견이 &quot;반대&quot;인 상황에, &quot;굳갱과 커뮤니티는 재원을 약속받았으니까&quot;라는 이유로 의견을 뒤집은 것은 핀시아 홀더 전체의 관점에서 봤을 때 일종의 배덕한 행위로 비춰지지 않나 싶습니다. 약 37억원 상당의 재원 중 70%는 커뮤니티에 사용하겠다고 하였지만, 그렇다면 나머지 30%에 해당하는 약 12억원은 &quot;찬성&quot;하였다는 발표만으로 굳갱랩스가 가져가게 되는 것입니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">또한, 26일 추가적으로 발표한 내용에 따르면 굳갱랩스와 의견이 일치하지 않는 스테이커들은 &quot;언스테이킹&quot;할 시간을 확보할 것이라고 밝혔습니다만, 실제로 반대의견을 가진 홀더들은 굳갱랩스의 노드에서 나와 26일 금일 오전 8시까지 네오핀 월렛으로 물량을 옮겨 투표권을 받은 뒤 투표를 진행했어야 하지만, FNSA의 언스테이킹에 7일이 소요되기 때문에, 물리적으로 투표권 행사가 불가능해졌습니다. 할 수 있는 건 네오핀 노드로 Redelegation한 후, 네오핀의 투표 결과를 기대하는 일밖에 할 수 없게 된 것입니다.<br /><br /></p>
+            <br /><br />
+            <h3 className="text-2xl font-bold my-6">핀시아 재단의 통합을 위한 총 공세<br /><br /></h3>
+            <br /><br />
+            <p className="mb-4">굳갱랩스의 발표 내용에는 &quot;굳갱랩스의 노드 스테이커에게 일정 재원을 분배한다&quot;라는 내용이 있습니다만, LINE측에서 운영중인 DOSI 노드에서도 금일 예치 이벤트를 실시했습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">19일 AMA에서 이뤄진 &quot;부결은 생각하고 있지 않다.&quot;라는 말이 새삼스럽게 느껴지는 대목입니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">아마도 어제(25일)까지는 핀시아 홀더 모두 &quot;자연스럽게 부결되거나, 핀시아 홀더 전체를 위한 개정안이 나올 것이다.&quot;라는 기대감이 있었으나, 마치 드라마처럼 개정안은 &quot;전체가 아닌 표를 위한 개정안&quot;으로 평가되고 있습니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">커뮤니티의 한 핀시아 홀더는 &quot;투자하고 있던 회사가 성장해 큰 기업과 합병하는 것은 무척 기쁜 일이다. 하지만, 회사의 성장을 도운 투자자에게 돌아갈 이익을 다른 곳에 돌린다면, 앞으로 누가 그 회사에 투자하고 기대할 것인가?&quot;라며 아쉬운 소회를 남겼습니다.<br /><br />
+                <br /><br /></p>
+            <br /><br />
+            <p className="mb-4">이로써 &quot;프로젝트 드래곤&quot;은 통합되는 방향으로 진행되고 있습니다. Web3 개발자의 입장에서는 역시나 통합으로 인한 EVM 생태계의 사용이 가능하다는 점에서 무척이나 기대가 되긴 합니다만, 통합의 과정에서 이렇게 여러가지 수단이 난무하고 이미지가 실추될 수 있는 과정이 포함되는 것은 다소 실망스러운 것도 사실입니다.<br /><br /></p>
+            <br /><br />
+            <p className="mb-4">차주 금요일인 2월 2일, 프로젝트 드래곤으로의 통합 투표 결과를 두고 보도록 하겠습니다.<br /><br /></p>
+            <br /><br />
+        </div>
+    )
+};
+
+export default MainAboutPage;
