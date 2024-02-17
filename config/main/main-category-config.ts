ebe44e646ced5703cc0f080bfe563218f227b23e
smarthug <kirklayer@gmail.com>
2024-02-17 18:56:28 +0900

    update

--- config/main/main-category-config.ts
+++ config/main/main-category-config.ts
@@ -15,53 +15,36 @@ const mainCategoryConfig: CategoryType[] = [
     icon: CategoryHomeIcon,
   },
   {
+    id: "market",
+    title: "Market",
+    slug: "market",
+    icon: CategoryScienceIcon,
+  },
+  {
+    id: "policy",
+    title: "Policy",
+    slug: "policy",
+    icon: CategoryScienceIcon,
+  },
+  {
+    id: "tech",
+    title: "tech",
+    slug: "tech",
+    icon: CategoryScienceIcon,
+  },
+  {
     id: "opinion",
     title: "Opinion",
     slug: "opinion",
     icon: CategoryScienceIcon,
   },
-  // {
-  //   id: "4db30a13-2797-4c7d-a0ce-e0c127287a39",
-  //   title: "Science",
-  //   slug: "science",
-  //   icon: CategoryScienceIcon,
-  // },
-  // {
-  //   id: "c13ae4a7-476c-4608-9b7a-9ec9488c42e4",
-  //   title: "Health",
-  //   slug: "health",
-  //   icon: CategoryHealthIcon,
-  // },
-  // {
-  //   id: "7b8781b0-b4fa-40e4-ac23-5310640eecd7",
-  //   title: "Marketing",
-  //   slug: "marketing",
-  //   icon: CategoryMarketingIcon,
-  // },
-  // {
-  //   id: "962f860d-ab0d-4650-ae93-8171c8b47169",
-  //   title: "Technology",
-  //   slug: "technology",
-  //   icon: CategoryTechnologyIcon,
-  // },
   {
     id: "dao",
     title: "Dao",
     slug: "dao",
     icon: CategoryScienceIcon,
   },
-  {
-    id: "technology",
-    title: "Technology",
-    slug: "technology",
-    icon: CategoryScienceIcon,
-  },
-  {
-    id: "research",
-    title: "Research",
-    slug: "research",
-    icon: CategoryScienceIcon,
-  },
+
   
 ];
 
