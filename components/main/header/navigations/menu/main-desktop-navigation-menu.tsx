325313929f3dfa27990a3510c5281c21839f033c
smarthug <kirklayer@gmail.com>
2024-03-09 13:25:37 +0900

    Refactor MainDesktopNavigationMenu component

--- components/main/header/navigations/menu/main-desktop-navigation-menu.tsx
+++ components/main/header/navigations/menu/main-desktop-navigation-menu.tsx
@@ -6,57 +6,83 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { v4 } from "uuid";
 
-const MainDesktopNavigationMenu = ({daoMember}) => {
+const MainDesktopNavigationMenu = ({ daoMember }) => {
   const currentPath = usePathname();
   return (
     <>
       <div className="hidden gap-x-6 md:flex">
-        {mainCategoryConfig.map((category) => (
-          <Link
-            href={
-              category.slug === "/"
-                ? category.slug
-                : `/category/${category.slug}`
+        {mainCategoryConfig.map((category) => {
+          if (category.title === "Dao") {
+            if (daoMember > 0) {
+              return (
+                <Link
+                  href={
+                    category.slug === "/"
+                      ? category.slug
+                      : `/category/${category.slug}`
+                  }
+                  key={v4()}
+                  className={cn(
+                    "relative inline-flex items-center rounded-full px-4 py-1.5 text-base font-semibold tracking-tight text-gray-500 antialiased ring-1 ring-transparent transition duration-200 [word-spacing:-5px] active:scale-[96%] active:ring-black/20",
+                    {
+                      "bg-gradient-to-r from-gray-200 via-gray-100 to-gray-50 px-4 text-gray-600 shadow-md shadow-black/5 ring-1 ring-black/10":
+                        currentPath ===
+                        (category.slug === "/"
+                          ? category.slug
+                          : `/category/${category.slug}`),
+                    },
+                    {
+                      "bg-transparent ring-transparent hover:bg-gradient-to-tr hover:from-gray-200 hover:via-gray-100 hover:to-gray-50 hover:shadow-md hover:shadow-black/5 hover:ring-1 hover:ring-black/10":
+                        currentPath !==
+                        (category.slug === "/"
+                          ? category.slug
+                          : `/category/${category.slug}`),
+                    },
+                  )}
+                >
+                  <div className="relative">{category.title}</div>
+                </Link>
+              );
+            } else {
+              return <></>;
             }
-            // href={
-              
-            //     category.slug
-                
-            // }
-            key={v4()}
-            className={cn(
-              "relative inline-flex items-center rounded-full px-4 py-1.5 text-base font-semibold tracking-tight text-gray-500 antialiased ring-1 ring-transparent transition duration-200 [word-spacing:-5px] active:scale-[96%] active:ring-black/20",
-              {
-                "bg-gradient-to-r from-gray-200 via-gray-100 to-gray-50 px-4 text-gray-600 shadow-md shadow-black/5 ring-1 ring-black/10":
-                  currentPath ===
-                  (category.slug === "/"
-                    ? category.slug
-                    : `/category/${category.slug}`),
-              },
-              {
-                "bg-transparent ring-transparent hover:bg-gradient-to-tr hover:from-gray-200 hover:via-gray-100 hover:to-gray-50 hover:shadow-md hover:shadow-black/5 hover:ring-1 hover:ring-black/10":
-                  currentPath !==
-                  (category.slug === "/"
-                    ? category.slug
-                    : `/category/${category.slug}`),
-              },
-            )}
-          >
-            {category.title === "Dao" ?
-              daoMember > 0 ?
-                <div className="relative">{category.title}</div>
-                :
-                <></>
-              :
+          }
+
+          return (
+            <Link
+              href={
+                category.slug === "/"
+                  ? category.slug
+                  : `/category/${category.slug}`
+              }
+              // href={
+
+              //     category.slug
+
+              // }
+              key={v4()}
+              className={cn(
+                "relative inline-flex items-center rounded-full px-4 py-1.5 text-base font-semibold tracking-tight text-gray-500 antialiased ring-1 ring-transparent transition duration-200 [word-spacing:-5px] active:scale-[96%] active:ring-black/20",
+                {
+                  "bg-gradient-to-r from-gray-200 via-gray-100 to-gray-50 px-4 text-gray-600 shadow-md shadow-black/5 ring-1 ring-black/10":
+                    currentPath ===
+                    (category.slug === "/"
+                      ? category.slug
+                      : `/category/${category.slug}`),
+                },
+                {
+                  "bg-transparent ring-transparent hover:bg-gradient-to-tr hover:from-gray-200 hover:via-gray-100 hover:to-gray-50 hover:shadow-md hover:shadow-black/5 hover:ring-1 hover:ring-black/10":
+                    currentPath !==
+                    (category.slug === "/"
+                      ? category.slug
+                      : `/category/${category.slug}`),
+                },
+              )}
+            >
               <div className="relative">{category.title}</div>
-            }
-          </Link>
-        ))}
-
-
-
-
-        
+            </Link>
+          );
+        })}
       </div>
     </>
   );
